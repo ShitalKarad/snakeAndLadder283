d4d8d1ad8585fe73b19b29db80e4ce98f1865b6c
ShitalKarad <shitalkarad24@gmail.com>
2023-05-16 21:59:29 +0530

    Added:inilize possion

---
+++ SnakeAndLadder.java
@@ -0,0 +1,18 @@
+package com.brigelabz;
+
+import java.util.Random;
+
+public class SnakeAndLadder {
+	
+	static final int initialPosition = 0;
+
+    public static void main(String[] args) {
+        System.out.println("Welcome in snake ladder program");
+        Random random = new Random();
+        int diceRoll = random.nextInt(7);
+
+        System.out.println("current position "+diceRoll);
+
+    }
+
+}
