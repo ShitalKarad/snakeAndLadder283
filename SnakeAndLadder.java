48ec5f8d44cd712133b49a27e976420c15f9cb0c
ShitalKarad <shitalkarad24@gmail.com>
2023-05-17 13:02:43 +0530

    Added:uc6 no.of times dice was played to win the game

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -4,21 +4,56 @@ import java.util.Random;
 
 public class SnakeAndLadder {
 	
-//roll die
-	
-	 static final int initialPosition = 0;
+//	Report the number of times the dice was played to win the game
+//	and also the position
+//	after every die role
+//	
+	static public final int Starting_Position_Of_Player = 0;
+	static public final int Ending_Position_Of_Player = 100;
+	public static final int ladder = 2;
+	public static final int snake = 1;
+	public static final int noPlay = 0;
+	static int count = 0;
 
-	    public static void main(String[] args) {
-	        System.out.println("Welcome in snake ladder program");
-	        Random random = new Random();
-	        int diceRoll = random.nextInt(7);
+	public static int rollDie() {
+		int dieValue = (int) (Math.random() * 10) % 6 + 1;
+		count++;
+		return dieValue;
+	}
 
-	        System.out.println("current position "+diceRoll);
+	public static void main(String[] args) {
+		System.out.println("Welcome in snake and ladder game");
+		int presentPlayerPosition = 0;
 
-	    }
+		while (presentPlayerPosition < Ending_Position_Of_Player) {
+			int currentPosition = 0;
+			int checkPosition = (int) (Math.random() * 10) % 3;
 
-  
-    }
+			switch (checkPosition) {
+			case snake:
+				currentPosition = rollDie();
+				if ((presentPlayerPosition - currentPosition) >= 0) {
+					presentPlayerPosition -= currentPosition;
+					System.out.println("player is on snake " + presentPlayerPosition);
+					
+				}
+				break;
+			case ladder:
+				currentPosition = rollDie();
+				if ((presentPlayerPosition + currentPosition) <= 100) {
+					presentPlayerPosition += currentPosition;
+					System.out.println("player is on Ladder " + presentPlayerPosition);
+					System.out.println("Number of time rollDie  "+count);
+				} 
+				break;
+			default:
+				System.out.println("player is not play ");
+			}
+
+		}
+
+	}
+}
 
 
 
